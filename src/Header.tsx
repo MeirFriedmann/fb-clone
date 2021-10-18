5630fb3051c6826d0fc8dae500dc8066d83df888
FriedmannMeir <friedmannmeir@gmail.com>
2021-10-19 00:04:50 +0300

    04

---
+++ src/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className="header__left">
+                <img
+                    src="https://logoeps.com/wp-content/uploads/2011/08/the-facebook-logo.jpg"
+                    alt=""
+                />
+            </div>
+            <h1></h1>
+            <div className="header__middle"></div>
+
+            <div className="header__right"></div>
+        </div>
+
+    )
+
+}
+
+export default Header
